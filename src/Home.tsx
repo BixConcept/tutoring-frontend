0d623c362c43c172435f0c5311b7dc518ea7957e
3nt3 <gott@3nt3.de>
2022-01-18 21:44:19 +0100

    make homepage responsive

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
       <section id="main-thingy">
         <div id="title">
           <h1>
-            <span>Nachhilfe&shy;plattform oder so</span>
+            <span className="backdrop">Nachhilfe&shy;plattform oder so</span>
           </h1>
         </div>
       </section>
@@ -20,11 +20,16 @@ function Home() {
             <p>
               Unsere Schule sieht sich als Europa-Schule mit Europa sehr stark
               verbunden. Außerdem hat unsere Schule 1000 verschiedene Siegel,
-              einfach nur weil wir es können. Unter anderem sind wir äußerst
-              stolz auf unser Fair-Trade Siegel und unser Berufswahl Siegel. Wir
-              sehen uns als Schule von Morgen und als ein Raum der
-              Lernentwicklung des einzelnen Individuums. Daher haben wir auf
-              Nachfrage der SV ein Ort geschaffen, wo Schüler Schülern helfen.
+              einfach nur weil wir es können.
+            </p>
+            <p>
+              Unter anderem sind wir äußerst stolz auf unser Fair-Trade Siegel
+              und unser Berufswahl Siegel. Wir sehen uns als Schule von Morgen
+              und als ein Raum der Lernentwicklung des einzelnen Individuums.
+              Daher haben wir auf Nachfrage der SV ein Ort geschaffen, wo
+              Schüler Schülern helfen.
+            </p>
+            <p>
               Wir haben eine Nachhilfeplattform geschaffen, wo jeder mitmachen
               kann. Brauchst du Nachhilfe? Wir finden einen netten Schüler der
               dir Nachhilfe gibt. Bist du ein netter Schüler? Wir brauchen dich!
@@ -35,7 +40,7 @@ function Home() {
           </div>
           <div id="home-choices">
             <div className="home-choice">
-              <a href="/register">
+              <a href="/find">
                 <h1>Ich brauche Nachhilfe</h1>
               </a>
             </div>
