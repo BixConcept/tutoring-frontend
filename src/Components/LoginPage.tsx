f2e572dc0dbbd7037f28fb4fd22cd7fd24c50f25
Ayberk <soysalayberk480@gmail.com>
2022-01-18 21:01:06 +0100

    Login Page mit einem tollen Auge

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons"; 
 
 function LoginPage() {
 		const [name, setName] = useState("");
@@ -17,6 +19,9 @@ function LoginPage() {
 				<input type="username" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" required
 				/>
 				<input type={pwType} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Passwort" required/>
+				<button onClick={(e) => {e.preventDefault(); changePwType()}}>
+				<FontAwesomeIcon icon={pwType == "password" ? faEye : faEyeSlash} />
+				</button>
 				<input type="submit" value="Login" />
 				</div>
 		)
