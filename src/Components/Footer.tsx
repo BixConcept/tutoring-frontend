2449b68dcf30dd3cab42d0ebf1b419a75e38d883
3nt3 <gott@3nt3.de>
2022-01-19 18:22:11 +0100

    add icon to source code in footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,11 +1,13 @@
 import css from "../styles/Footer.module.scss";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
     <footer className={css.footer}>
       <a href="https://github.com/HaanerBarbaren/tutoring-frontend">
-        Source Code
+        Source Code <FontAwesomeIcon icon={faCodeBranch} />
       </a>
       <Link to="/license">Lizenz</Link>
       <Link to="/imprint">Impressum</Link>
