32e27746f8ab15c6408b70c0584c874d56efad20
Laurens <laurens.heithecker@outlook.com>
2022-01-21 13:12:45 +0100

    adding lottie animations

--- src/pages/Imprint.tsx
+++ src/pages/Imprint.tsx
@@ -1,9 +1,24 @@
+import lottie from "lottie-web";
+import { useEffect, useRef } from "react";
 import Page from "../Components/Page";
 
 function Imprint() {
+  const container = useRef(null);
+
+  useEffect(() => {
+    if (container.current)
+      lottie.loadAnimation({
+        container: container.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: require("./office.json"),
+      });
+  }, []);
+
   return (
     <Page title="Impressum">
-      <h1>noch nix drin...</h1>
+      <div className="container" ref={container}></div>
     </Page>
   );
 }
