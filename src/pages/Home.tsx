6ef61f40f6a080ca5754c6af0ce59ca1b36a262c
3nt3 <gott@3nt3.de>
2022-01-21 15:51:51 +0100

    turn <a>s into <Link>s to make them SPA

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import css from "../styles/Home.module.scss";
+import { Link } from "react-router-dom";
 
 function Home() {
   document.title = "Nachhilfe GymHaan";
@@ -41,16 +42,16 @@ function Home() {
           </div>
           <div id={css.choices}>
             <div className={css.choice}>
-              <a href="/find">
+              <Link to="/find">
                 <h1>Ich brauche Nachhilfe</h1>
-              </a>
+              </Link>
             </div>
             <div className={css.choice}>
-              <h1>
-                <a href="/register">
+              <Link to="/register">
+                <h1>
                   Ich möchte <span>Nachhilfe geben</span>
-                </a>
-              </h1>
+                </h1>
+              </Link>
             </div>
           </div>
         </div>
