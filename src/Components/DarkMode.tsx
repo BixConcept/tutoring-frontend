34ce7e733a9cecec17e835dbcb3b52acfc0d1c98
Sanberk Soysal <sanberk@sanberk.xyz>
2022-01-24 16:24:34 +0100

    fix dark mode bug when reloading the page
    Co-authored-by: Ayberk Soysal <soysalayberk480@gmail.com>
    Signed-off-by: Sanberk Soysal <sanberk@sanberk.xyz>

--- src/Components/DarkMode.tsx
+++ src/Components/DarkMode.tsx
@@ -52,7 +52,7 @@ const DarkMode = () => {
               onChange={() => {
                 toggleDarkMode(setTheme);
               }}
-              checked={theme === "dark"}
+              checked={localStorage.getItem("theme") === "dark"}
             />
           )}
         </ThemeContext.Consumer>
