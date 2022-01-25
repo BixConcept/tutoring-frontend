5707cf9e0b483d16309b00e5cd90c93b46fb1e7a
3nt3 <gott@3nt3.de>
2022-01-25 15:31:41 +0100

    add padding and validate inputs on /find

--- src/pages/Find.tsx
+++ src/pages/Find.tsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import css from "../styles/findPage.module.scss";
 import { ThemeContext } from "../ThemeContext";
 import grades from "../pages/RegisterPage";
+import { toast, ToastContainer } from "react-toastify";
 
 function Find() {
   document.title = "Nachhilfe finden";
@@ -39,53 +40,79 @@ function Find() {
     }
   }
 
-  const [stufe, setStufe] = useState("");
-  const [fach, setFach] = useState("");
+  const [grade, setGrade] = useState("");
+  const [subject, setSubject] = useState("");
+
+  function validate() {
+    if (grade === "") {
+      toast.error("Du musst eine Stufe auswählen", {
+        position: "bottom-right",
+        theme:
+          context.theme === "dark" || context.theme === "light"
+            ? context.theme
+            : "dark",
+      });
+    }
+    if (subject === "") {
+      toast.error("Du musst ein Fach auswählen", {
+        position: "bottom-right",
+        theme:
+          context.theme === "dark" || context.theme === "light"
+            ? context.theme
+            : "dark",
+      });
+    }
+  }
 
   return (
     <div className={css.container}>
-      <h1>Nachhilfe finden</h1>
-      <div className={css.inputfields}>
-        <form
-          onSubmit={(e) => {
-            // Suche
-            e.preventDefault();
-          }}
-        >
-          <div id={css.inputRow}>
-            <div>
-              <label htmlFor="subject">Fach auswählen</label>
-              <div className={css.inputField}>
-                <select
-                  name="subject"
-                  id=""
-                  onChange={(e) => setFach(e.target.value)}
-                >
-                  <option value="">--- Fach wählen ---</option>
-                  {subjects.sort().map((subject, index) => {
-                    return <option key={index}>{subject}</option>;
-                  })}
-                </select>
+      <div className={css.formContainer}>
+        <h1>Nachhilfe finden</h1>
+        <div className={css.inputfields}>
+          <form
+            onSubmit={(e) => {
+              validate();
+
+              // Suche
+              e.preventDefault();
+            }}
+          >
+            <div id={css.inputRow}>
+              <div>
+                <label htmlFor="subject">Fach auswählen</label>
+                <div className={css.inputField}>
+                  <select
+                    name="subject"
+                    id=""
+                    onChange={(e) => setSubject(e.target.value)}
+                  >
+                    <option value="">--- Fach wählen ---</option>
+                    {subjects.sort().map((subject, index) => {
+                      return <option key={index}>{subject}</option>;
+                    })}
+                  </select>
+                </div>
+              </div>
+              <div>
+                <label htmlFor="subject">Deine Stufe</label>
+                <div className={css.inputField}>
+                  <select
+                    name=""
+                    id=""
+                    onChange={(e) => setGrade(e.target.value)}
+                  >
+                    <option value="">--- Stufe wählen ---</option>
+                    {grades.map((grade, index) => {
+                      return <option key={index}>{grade}</option>;
+                    })}
+                  </select>
+                </div>
               </div>
             </div>
-            <div>
-              <label htmlFor="subject">Deine Stufe</label>
-              <div className={css.inputField}>
-                <select
-                  name=""
-                  id=""
-                  onChange={(e) => setStufe(e.target.value)}
-                >
-                  <option value="">--- Stufe wählen ---</option>
-                  {grades.map((grade, index) => {
-                    return <option key={index}>{grade}</option>;
-                  })}
-                </select>
-              </div>
-            </div>
-          </div>
-          <input type="submit" value="weiter" id={css.submit} />
-        </form>
+            <input type="submit" value="suchen 🚀" id={css.submit} />
+          </form>
+          <ToastContainer />
+        </div>
       </div>
     </div>
   );
