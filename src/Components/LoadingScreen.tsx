2f379bd8a1d605421176c011af8e55bea5a67ca6
Laurens <laurens.heithecker@outlook.com>
2022-02-08 22:02:59 +0100

    loading at license fixed and darkmode emoji for apple devices fixed

--- src/Components/LoadingScreen.tsx
+++ src/Components/LoadingScreen.tsx
@@ -1,18 +1,27 @@
 import Logo from "../assets/images/logo.svg";
 import css from "../styles/LoadingScreen.module.scss";
+import { useRef, useEffect } from "react";
+import lottie from "lottie-web";
 
 function LoadingScreen(props: { s: boolean }) {
+  // loading animation
+  const loading = useRef(null);
+
+  useEffect(() => {
+    if (loading.current) {
+      lottie.loadAnimation({
+        container: loading.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: require("../assets/animations/loading.json"),
+      });
+    }
+  });
   if (!props.s) {
     return (
-      <div className={css.LoadingScreen}>
-        <div className={css.spinner}>
-          <img
-            src={Logo}
-            className={css.LoadingSpin}
-            alt="spin"
-            style={{ opacity: props.s ? 0 : 1 }}
-          />
-        </div>
+      <div className={css.LoadingScreen} style={{ opacity: props.s ? 0 : 1 }}>
+        <div className={css.loading_animation} ref={loading}></div>
       </div>
     );
   } else return null;
