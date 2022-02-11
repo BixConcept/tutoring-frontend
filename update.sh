52319a1da42ee7efb63f7c3189eccc3df8c3f318
3nt3 <gott@3nt3.de>
2022-02-11 17:56:18 +0100

    update update.sh
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ if [[ $(git pull) != "Already up to date." ]]
 then
 	npm install 
 	npm run build 
+    rm -rf build_production
     mv build build_production # we serve build_production so the user doesn't get a 404 while updating
 fi
 
