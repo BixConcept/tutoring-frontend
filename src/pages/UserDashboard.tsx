0a312b3cb5cdc830c4ec5232841cfd39183c172f
3nt3 <gott@3nt3.de>
2022-02-12 02:55:53 +0100

    monster commit that makes the dashboard functional

--- src/pages/UserDashboard.tsx
+++ src/pages/UserDashboard.tsx
@@ -45,62 +45,96 @@ const UserDashboard = (): JSX.Element => {
     <div id={css.dashboard}>
       <div id={css["dashboard-content"]}>
         <h1>Dashboard</h1>
-        <h2>Account löschen</h2>
-        <button
-          onClick={(e) => setModalVisible(!modalVisible)}
-          className={general["text-button"]}
-        >
-          Löschen
-        </button>
-        <div
-          id={css["modal-background"]}
-          style={{ display: modalVisible ? "flex" : "none" }}
-        >
-          <div id={css["modal-content"]} onClick={() => {}}>
-            <div id={css["modal-heading-row"]}>
-              <h1>Account löschen</h1>
-              <button
-                onClick={() => setModalVisible(false)}
-                id={css["close-button"]}
-              >
-                &times;
-              </button>
+        <h4>Meine Fächer</h4>
+        <i>Coming soon...</i>
+
+        <h4>Danger Zone</h4>
+        <div id={css["danger-zone"]}>
+          <div className={css["danger-item"]}>
+            <div>
+              <p className={css["danger-action"]}>Diesen Account löschen</p>
+              <p className={css["danger-description"]}>
+                Sobald du deinen Account gelöscht hast, wirst du nicht mehr in
+                der Suche angezeigt und kannst nicht mehr darauf zugreifen.
+              </p>
             </div>
-            <p>Diese Aktion ist ir­re­ver­si­bel!</p>
-            <p>
-              Bitte gib zum Bestätigen{" "}
-              <span id={css.name}>{context.user?.name}</span> ein.
-            </p>
-            {/* this form is there so the user can submit via pressing escape or the key on their mobile keyboard */}
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                fetch(`${API_HOST}/user`, {
-                  method: "DELETE",
-                  credentials: "include",
-                }).then((res) => {
-                  if (!res.ok) {
-                    Alert("Irgendwas ist schiefgegangen.", "error", getTheme());
-                  } else {
-                    Alert("Account gelöscht.", "info", getTheme());
-                  }
-                });
-              }}
+            <button
+              onClick={(e) => setModalVisible(!modalVisible)}
+              className={general["text-button"]}
             >
-              <input
-                type="text"
-                placeholder=""
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
+              Account löschen
+            </button>
+          </div>
+          <div
+            id={css["modal-background"]}
+            style={{ display: modalVisible ? "flex" : "none" }}
+          >
+            <div id={css["modal-content"]} onClick={() => {}}>
+              <div id={css["modal-heading-row"]}>
+                <h1>Account löschen</h1>
+                <button
+                  onClick={() => setModalVisible(false)}
+                  id={css["close-button"]}
+                >
+                  &times;
+                </button>
+              </div>
+              <p>Diese Aktion ist ir­re­ver­si­bel!</p>
+              <p>
+                Bitte gib zum Bestätigen{" "}
+                <span id={css.name}>{context.user?.name}</span> ein.
+              </p>
+              {/* this form is there so the user can submit via pressing escape or the key on their mobile keyboard */}
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  fetch(`${API_HOST}/user`, {
+                    method: "DELETE",
+                    credentials: "include",
+                  }).then((res) => {
+                    if (!res.ok) {
+                      Alert(
+                        "Irgendwas ist schiefgegangen.",
+                        "error",
+                        getTheme()
+                      );
+                    } else {
+                      Alert("Account gelöscht.", "info", getTheme());
+                    }
+                  });
                 }}
-              />
-              <input
-                type="submit"
-                value="Löschen"
-                disabled={name !== context.user?.name}
-              />
-            </form>
+              >
+                <input
+                  type="text"
+                  placeholder=""
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+                <input
+                  type="submit"
+                  value="Löschen"
+                  disabled={name !== context.user?.name}
+                />
+              </form>
+            </div>
+          </div>
+          <hr />
+          <div className={css["danger-item"]}>
+            <div>
+              <p className={css["danger-action"]}>E-Mail ändern</p>
+              <p className={css["danger-description"]}>
+                Joah keine Ahnung du musst halt ne andere E-Mail benutzen.
+              </p>
+            </div>
+            <button
+              onClick={(e) => setModalVisible(!modalVisible)}
+              className={general["text-button"]}
+              disabled
+            >
+              E-Mail ändern
+            </button>
           </div>
         </div>
       </div>
