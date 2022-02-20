9e4979b215d3f02405f1f1be0cc96758bd7b9ab4
3nt3 <gott@3nt3.de>
2022-02-20 21:33:23 +0100

    important
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/pages/Imprint.tsx
+++ src/pages/Imprint.tsx
@@ -1,11 +1,27 @@
 import Page from "../Components/Page";
+import { useState } from "react";
+import BixConcept from "../assets/images/bixconcept.svg";
+import css from "../styles/Imprint.module.scss";
 
 const Imprint = (): JSX.Element => {
   let imprint = "https://gymhaan.de/unsere-schule/impressum/";
+  const [clickCount, setClickCount] = useState(0);
+
   return (
     <Page title="Impressum">
-      Das Impressum unserer Schule ist zu finden auf:{" "}
-      <a href={imprint}>{imprint}</a>
+      <span
+        onClick={() => {
+          setClickCount(clickCount + 1);
+        }}
+      >
+        Das Impressum
+      </span>{" "}
+      unserer Schule ist zu finden auf: <a href={imprint}>{imprint}</a>
+      {clickCount >= 10 ? (
+        <a href="https://bixconcept.tech">
+          <img src={BixConcept} alt="BixConcept" id={css.img} />
+        </a>
+      ) : null}
     </Page>
   );
 };
