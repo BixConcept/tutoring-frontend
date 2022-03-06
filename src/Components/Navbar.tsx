945fe7fab1120768d5f0a87b8dd527669a162a70
3nt3 <gott@3nt3.de>
2022-03-06 19:19:07 +0100

    build version 1.0
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,7 @@
 import css from "../styles/Navbar.module.scss";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { OurContext } from "../OurContext";
 import { API_HOST } from "..";
 import Alert from "./Alert";
@@ -10,14 +10,24 @@ import DarkMode from "./DarkMode";
 
 export default function Navbar() {
   const context = useContext(OurContext);
+  const [count, setCount] = useState<number>(0);
+  const [lastClick, setLastClick] = useState<Date>(new Date());
 
   return (
     <nav>
       <ul>
-        <li id={css.logo}>
+        <li
+          id={css.logo}
+          onClick={() => {
+            setCount(
+              new Date().getTime() - lastClick.getTime() < 1000 ? count + 1 : 0
+            );
+            setLastClick(new Date());
+          }}
+        >
           <Link to="/">
             <img src={logo} alt="gymhaan logo" />
-            <span>powered by Bix|Concept</span>
+            {count > 20 ? <span>powered by Bix|Concept</span> : null}
           </Link>
         </li>
         <div id={css.links}>
