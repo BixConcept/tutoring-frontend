4ab8989a47057f1922dbcf6a0c035327584aa55a
Sanberk Soysal <sanberk@sanberk.xyz>
2022-04-13 18:19:18 +0200

    dinge am footer verändert...
    Signed-off-by: Sanberk Soysal <sanberk@sanberk.xyz>

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -12,15 +12,15 @@ const Footer = (): JSX.Element => {
             Source Code <FontAwesomeIcon icon={faGithub} />
           </a>
         </li>
-        <li>
+        <li className={css.hochlinie}>
           <a href="https://github.com/BixConcept/tutoring-frontend/blob/main/LICENSE">
             Lizenz
           </a>
         </li>
-        <li>
+        <li className={css.hochlinie}>
           <Link to="/privacy">Datenschutz</Link>{" "}
         </li>
-        <li>
+        <li className={css.hochlinie}>
           <Link to="/imprint">Impressum</Link>
         </li>
       </ul>
